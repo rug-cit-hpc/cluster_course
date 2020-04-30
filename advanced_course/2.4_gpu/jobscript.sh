c64da2b376db3c4330733e00a2add67b55652073
Fokke Dijkstra <f.dijkstra@rug.nl>
2020-04-30 17:07:11 +0200

    Added loading of the PGI compiler to the jobscript file.

--- advanced_course/2.4_gpu/jobscript.sh
+++ advanced_course/2.4_gpu/jobscript.sh
@@ -8,6 +8,9 @@
 #SBATCH --job-name=Blurring
 #SBATCH --output=mpi.out
 
+module purge
+module load PGI/19.10-GCC-8.3.0-2.32 
+
 convert Microcrystals.jpg Microcrystals.rgb
 ./mpi_omp_conv Microcrystals.rgb 5184 3456 50 rgb
 convert -size 5184x3456  -depth 8 conv_Microcrystals.rgb conv_Microcrystals.jpg
