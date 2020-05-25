638b204d650a9bdab237e8e0dd17a40ac7d1c87e
Fokke Dijkstra <f.dijkstra@rug.nl>
2020-05-25 12:13:46 +0200

    Added quotation marks to filenames.

--- advanced_course/1.7_jobarray/solution/jobscript.sh
+++ advanced_course/1.7_jobarray/solution/jobscript.sh
@@ -5,30 +5,30 @@
 #SBATCH --mem=4GB
 #SBATCH --time=00:10:00
 #SBATCH --partition=short
-#SBATCH --job-name=Blurring
+#SBATCH --job-name=Edge_Detection
 #SBATCH --array=1-31
 
 # Check if filename has been supplied
-if [ -z $1 ]
+if [ -z "$1" ]
 then
     echo "ERROR: No input specified"
     exit -1
 fi
 
 # Check if file exists
-if [ -e $1 ]
+if [ -e "$1" ]
 then
     # Find the n-th image in the input file
-    image=$(cat $1 | head -n ${SLURM_ARRAY_TASK_ID} | tail -n 1)
-    echo "Processing image: " $1
+    image=$(cat "$1" | head -n ${SLURM_ARRAY_TASK_ID} | tail -n 1)
+    echo "Processing image: " "$1"
 
     # Get the directory in which the file is stored
-    dirname=$( dirname $image)
-    filename=$( basename $image)
+    dirname=$( dirname "$image")
+    filename=$( basename "$image")
     
     # Determine width and height of the image
-    width=$(identify -format "%w" $dirname/$filename)
-    height=$(identify -format "%h" $dirname/$filename)
+    width=$(identify -format "%w" "$dirname/$filename")
+    height=$(identify -format "%h" "$dirname/$filename")
     echo "Width: " $width
     echo "Height: " $height
     
@@ -38,14 +38,14 @@ then
     module load foss/2018a
 
     # Convert the jpg file to the rgb format for easy processing
-    convert $dirname/$filename $filename.rgb
+    convert "$dirname/$filename" "$filename.rgb"
     # Run the convolution filter program on the image
-    ./mpi_omp_conv $filename.rgb $width $height 1 rgb
+    ./mpi_omp_conv "$filename.rgb" $width $height 1 rgb
     # Convert the resulting file back to jpg format
-    convert -size ${width}x${height}  -depth 8 conv_$filename.rgb conv_$filename
+    convert -size ${width}x${height}  -depth 8 "conv_$filename.rgb" "conv_$filename"
     
     # Remove the intermediate files
-    rm $filename.rgb conv_$filename.rgb
+    rm "$filename.rgb" "conv_$filename.rgb"
 else
     echo "ERROR: File does not exist"
     exit -1
