d45d15ad6afa3735b30cc62339003e7c517db7d0
Neves-P <p.m.santos.neves@rug.nl>
2023-09-26 11:23:34 +0200

    Revise ex 1.1

--- advanced_course/1.1_jobscript/jobscript.sh
+++ advanced_course/1.1_jobscript/jobscript.sh
@@ -4,22 +4,26 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4GB
 #SBATCH --time=00:10:00
-#SBATCH --partition=short
+#SBATCH --partition=regular
 #SBATCH --job-name=Edge_detection
 
 # Clean up the module environment
 module purge
 # Load the compilers
-module load foss/2020a
+module load foss/2022b
+# Load the conversion tool
+module load ImageMagick/7.1.0-53-GCCcore-12.2.0
 # Compile the program
 make
 
+echo "Processing image: " $1
+
 # Convert the jpg file to the rgb format for easy processing
-convert Microcrystals.jpg Microcrystals.rgb
+convert $1 $1.rgb
 # Run the convolution filter program on the image
-./mpi_omp_conv Microcrystals.rgb 5184 3456 1 rgb
+./mpi_omp_conv $1.rgb 5184 3456 1 rgb
 # Convert the resulting file back to jpg format
-convert -size 5184x3456  -depth 8 conv_Microcrystals.rgb conv_Microcrystals.jpg
+convert -size 5184x3456  -depth 8 conv_$1.rgb conv_$1
 
 # Remove the intermediate files
-rm Microcrystals.rgb conv_Microcrystals.rgb
+rm $1.rgb conv_$1.rgb
